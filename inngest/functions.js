0d63ec9c07f63e82df84a37a213ed3b7b21c65f6
hafidha <hafidha.bouabdelli@gmail.com>
2025-09-15 16:05:04 +0100

    project setup

---
+++ inngest/functions.js
@@ -0,0 +1,59 @@
+import { inngest } from "./client";
+import prisma from "@/lib/prisma";
+
+// console.log("the file is loaded");
+
+export const syncUserCreation = inngest.createFunction(
+  { id: "sync-user-create" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    console.log("Received user.created event:", event.data);
+
+    const data = event.data;
+
+    try {
+      await prisma.user.create({
+        data: {
+          id: data.id,
+          email: data.email_addresses[0]?.emailAddress ?? "",
+          name: `${data.first_name ?? ""} ${data.last_name ?? ""}`.trim(),
+          image: data.image_url ?? "",
+        },
+      });
+      console.log("User inserted into Neon:", data.id);
+    } catch (err) {
+      console.error("Failed to insert user into Neon:", err);
+    }
+  }
+);
+
+export const syncUserUpdate = inngest.createFunction(
+  { id: "sync-user-update" },
+  { event: "clerk/user.updated" },
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.user.update({
+      where: { id: data.id },
+      data: {
+        id: data.id,
+        email: data.email_addresses[0]?.emailAddress,
+        name: `${data.first_name} ${data.last_name}`,
+        image: data.image_url,
+      },
+    });
+
+    return;
+  }
+);
+export const syncUserDeletion = inngest.createFunction(
+  { id: "sync-user-delete" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.user.delete({
+      where: { id: data.id },
+    });
+
+    return;
+  }
+);
